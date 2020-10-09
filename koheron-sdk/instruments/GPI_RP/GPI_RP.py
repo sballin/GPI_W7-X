0563b1bf72ca0477dec137acbdc5c7b809a48b74
Sean Ballinger <sean.ballinger@gmail.com>
2020-10-09 13:12:08 -0400

    Fix line spacing

--- koheron-sdk/instruments/GPI_RP/GPI_RP.py
+++ koheron-sdk/instruments/GPI_RP/GPI_RP.py
@@ -17,11 +17,9 @@ class GPI_RP(object):
         self.adc = np.zeros((2, self.n_pts))
         self.dac = np.zeros((2, self.n_pts))
 
-
     @command()
     def set_led(self, led):
         pass
-
 
     @command()
     def set_analog_out(self, analog_out):
@@ -31,71 +29,53 @@ class GPI_RP(object):
         '''
         pass
 
-
     @command()
     def get_fifo_length(self):
         return self.client.recv_uint32()
 
-
     @command()
     def get_buffer_length(self):
         return self.client.recv_uint32()
 
-
     @command()
     def get_GPI_data(self):
         return self.client.recv_vector(dtype='uint32')
-        
-        
-    @command()
-    def get_buffer_length(self):
-        return self.client.recv_uint32() 
-
 
     @command()
     def reset_fifo(self):
         pass
 
-
     @command()
     def set_GPI_safe_state(self, state):
         pass
-
 
     @command()
     def set_slow_1(self, state):
         pass
 
-
     @command()
     def set_slow_2(self, state):
         pass
-
 
     @command()
     def set_slow_3(self, state):
         pass
 
-
     @command()
     def set_slow_4(self, state):
         pass
-
 
     @command()
     def reset_time(self, state):
         pass
 
-
     @command()
     def set_fast(self, state):
         pass
 
-
     @command()
     def set_fast_permission_1(self, state):
         pass
-
 
     @command()
     def set_fast_delay_1(self, state):
@@ -116,8 +96,7 @@ class GPI_RP(object):
     @command()
     def set_fast_duration_2(self, state):
         pass
-        
-        
+         
     @command()
     def send_T1(self, state):
         pass
@@ -125,52 +104,42 @@ class GPI_RP(object):
     @command()
     def get_W7X_permission(self):
         return self.client.recv_uint32()
-
-
+    
     @command()
     def get_analog_out(self):
         return self.client.recv_uint32()
-
 
     @command()
     def get_abs_gauge(self):
         return self.client.recv_uint32()
 
-
     @command()
     def get_diff_gauge(self):
         return self.client.recv_uint32()
-
 
     @command()
     def get_analog_input_0(self):
         return self.client.recv_uint32()
 
-
     @command()
     def get_analog_input_1(self):
         return self.client.recv_uint32()
-
 
     @command()
     def get_slow_1_sts(self):
         return self.client.recv_uint32()
 
-
     @command()
     def get_slow_2_sts(self):
         return self.client.recv_uint32()
-
 
     @command()
     def get_slow_3_sts(self):
         return self.client.recv_uint32()
         
-
     @command()
     def get_slow_4_sts(self):
         return self.client.recv_uint32()
-
 
     @command()
     def get_fast_sts(self):
