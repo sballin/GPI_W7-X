fc5ab1f519f12c5c4d6f065db7f8596e0d076360
Sean Ballinger <sean.ballinger@gmail.com>
2021-03-29 14:52:32 -0400

    Fix python error during build

--- koheron-sdk/python/python.mk
+++ koheron-sdk/python/python.mk
@@ -1,6 +1,6 @@
 
-KOHERON_PYTHON_VERSION := $(shell cd python && python -c "from koheron.version import __version__; print(__version__)")
-PYPI_VERSION := $(shell curl -s 'https://pypi.org/pypi/koheron/json'| PYTHONIOENCODING=utf8 python -c "import sys, json; print(json.load(sys.stdin)['info']['version'])")
+KOHERON_PYTHON_VERSION := $(shell cd python && python2 -c "from koheron.version import __version__; print(__version__)")
+PYPI_VERSION := $(shell curl -s 'https://pypi.org/pypi/koheron/json'| PYTHONIOENCODING=utf8 python2 -c "import sys, json; print(json.load(sys.stdin)['info']['version'])")
 
 PHONY: upload_pip
 upload_pip:
@@ -10,6 +10,6 @@ upload_pip:
 	rm -rf python/dist
 	rm -rf python/koheron.egg-info
 ifneq ($(PYPI_VERSION), $(KOHERON_PYTHON_VERSION))
-	cd python && python setup.py sdist bdist_wheel
+	cd python && python2 setup.py sdist bdist_wheel
 	twine upload -u $(PYPI_USERNAME) -p $(PYPI_PASSWORD) python/dist/*
 endif
